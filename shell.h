a33e3c9b17c81b6418c40f08e56253ec77c937c1
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-06-07 22:03:10 +0200

    shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#endif
