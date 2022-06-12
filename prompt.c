209e763b66a410ee787f654c72c3015be83cc2b6
Edward kataika <ekataika2002@gmail.com>
2022-06-12 21:07:57 +0200

    betty fix

--- prompt.c
+++ prompt.c
@@ -1,26 +1,30 @@
-include "shell.h"
+#include "shell.h"
+/**
+ * main - prints the prompt and gets the command
+ *
+ *@args: argument 1
+ *@argsv: argument 2
+ *
+ *Return: 0 on success
+ */
 
 int main(int args, char **argsv)
 {
 	int BUFFERSIZE = 1024;
 	char cmdline[BUFFERSIZE];
-	
-	while (1)
-	{
-		printf("user:> ");
-		
-		if ((fgets(cmdline,BUFFERSIZE,stdin) == NULL) && ferror(stdin))
-		error("fgets error");
-		
-		if (feof(stdin))
-		{
-			printf("\n");
-			exit(0);
-		}
-		
-		cmdline[strlen(cmdline)-1] = "\0";
-		
-		eval(cmdline);
-	}
 
-} 
+while (1)
+	{
+	printf("user:>");
+	if ((fgets(cmdline, BUFFERSIZE, stdin) == NULL) && ferror(stdin))
+		error("fgets error");
+	if (feof(stdin))
+	{
+	printf("\n");
+	exit(0);
+	}
+	cmdline[strlen(cmdline) - 1] = "\0";
+	eval(cmdline);
+	}
+return (0);
+}
