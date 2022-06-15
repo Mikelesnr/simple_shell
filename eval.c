8b2573baff1ac381011d3ad1f609f61beaac46e1
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-06-15 21:34:00 +0200

    fork.c

--- eval.c
+++ eval.c
@@ -9,12 +9,18 @@
 
 void eval(char *cmdline)
 {
-int bg;
-struct command cmd;
+	int bg;
+	struct command cmd;
 
-printf("evaluating '%s'\n", cmdline);
-bg = parse(cmdline, *cmd);
-printf("Found command %s\n", cmd.argv[0]);
-if (bg == -1)
-return;
+	printf("evaluating '%s'\n", cmdline);
+	bg = parse(cmdline, *cmd);
+	printf("Found command %s\n", cmd.argv[0]);
+	if (bg == -1)return;
+
+	if (cmd.argv[0] == NULL) return;
+
+	if (cmd.builtin == NONE)
+		runSystemCOmmand(&cmd, bg);
+	else
+		runBuiltinCommand (&cmd, bg);
 }
