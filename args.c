e2b8c24774d92301e26ed48a6c4b0c4cc1e2611e
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-06-15 22:59:47 +0200

    shell.h

--- args.c
+++ args.c
@@ -1,4 +1,4 @@
-include "shell.h"
+#include "shell.h"
 
 int main(int args, char *argv[])
 {
--- shell.h
+++ shell.h
@@ -1,30 +1,34 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-/* header files */
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <dirent.h>
+#include <limits.h>
+#include <string.h>
 
-int MAXARGS;
-struct command {
-int args;
-char *argv[MAXARGS];
-enum builtin_t{
-NUN, QUIT, JOBS< BG, FG} buitin;
+extern char **environ;
 
-int main(int args, char **argsv)
-void eval(char *cmdline)
-int main(int args, char *argv[])
-Int parse(const char *cmdline, struct command *cmd)
-void runSystemCommand(struct command *cmd, int bd)
-
+char *show_input(void);
+void prompt(void);
+char *_strcat(char *src);
+int _strlen(char *str);
+void place(char *str);
+char *findfile(char *command);
+char *find_command(char *command);
+int compare(char *s1, char *s2);
+int _strcmpdir(char *s1, char *s2);
+int charput(char c);
+void place(char *str);
+char *str_concat(char *s1, char *s2);
+int lookforslash(char *cmd);
+int compareExit(char *s1, char *s2);
+int compareEnv(char *s1, char *s2);
+void execute_proc(char **cmd);
+char **identify_string(char *parameter);
+void controlC(int sig);
 #endif
-
