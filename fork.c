4342725a920a62bf606df455b711fe9bca8184a9
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-06-15 23:44:40 +0200

    fork.c

--- fork.c
+++ fork.c
@@ -1,13 +1,99 @@
-include "shell.h"
+#include "shell.h"
 
-int main ()
+/**
+ * lookforslash - identifies if first char is a slash.
+ * @cmd: first string
+ * Return: 1 if yes 0 if no.
+ */
+int lookforslash(char *cmd)
 {
-	pid_t childPid;
-	printf("Before fork %d\n", getpid());
+	int cont = 0;
 
-	//fork creates a child and returns its process id
-	childPid = fork();
+	while (cmd[cont])
+	{
+		if (cmd[0] == '/')
+		{
+			printf("%c\n", cmd[0]);
+			return (1);
+		}
 
-	printF("After fork %d\n", getpid())
-		exit(0);
+		cont++;
+	}
+	return (0);
+}
+
+/**
+ * compareExit - identifies if first char is a slash.
+ * @s1: first string
+ * @s2: exit string
+ * Return: 1 if yes 0 if no.
+ */
+int compareExit(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+	{
+		if (i == 3)
+			break;
+		i++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
+/**
+ * compareEnv - identifies if first char is a slash.
+ * @s1: first string
+ * @s2: exit string
+ * Return: 1 if yes 0 if no.
+ */
+int compareEnv(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+	{
+		if (i == 2)
+			break;
+		i++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+/**
+ * identify_string - identyfy keyboard input.
+ * @parameter: call prompt from another function (prompt)
+ * Return: str
+ **/
+char **identify_string(char *parameter)
+{
+	char **buf = malloc(1024 * sizeof(char *));
+	char *split;
+	int i = 0;
+	char *delim = " \t\n";
+
+
+	split = strtok(parameter, delim);
+
+	while (split != NULL)
+	{
+		buf[i] = split;
+		i++;
+		split = strtok(NULL, delim);
+	}
+	execute_proc(buf);
+	return (buf);
+
+}
+/**
+ * controlC - avoid close the shell
+ * @sig: keep going shell
+ **/
+void  controlC(int sig)
+{
+	(void) sig;
+	write(1, "\n$ ", 3);
 }
