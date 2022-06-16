0076049dd3edf8de55be86cead876906d5f994a4
Edward kataika <ekataika2002@gmail.com>
2022-06-16 21:34:10 +0200

    rinsy fix

--- runSyCom.c
+++ runSyCom.c
@@ -1,46 +1,95 @@
 #include "shell.h"
+/**
+ * _getenv - gets the path
+ * @path_name: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+char *_getenv(char *path_name)
+{
+	char **environ_cursor, *env_ptr, *name_ptr;
+
+	environ_cursor = environ;
+	while (*environ_cursor)
+	{
+		env_ptr = *environ_cursor;
+		name_ptr = path_name;
+		while (*env_ptr == *name_ptr)
+		{
+			if (*env_ptr == '=')
+				break;
+			env_ptr++;
+			name_ptr++;
+		}
+		if ((*env_ptr == '=') && (*name_ptr == '\0'))
+			return (env_ptr + 1);
+		environ_cursor++;
+	}
+	return (NULL);
+}
+/**
+ * signal_handler - handle the process interrept signal
+ * @signo: the signal identifier
+ *
+ * Return: void
+ */
+void signal_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		PRINT("\n");
+		PRINT(PROMPT);
+	}
+}
+/**
+ * fill_an_array - fill an array with elements
+ * @a: the given array
+ * @el: the given element
+ * @len: the length of the array
+ *
+ * Return: pointer to filled array
+ */
+void *fill_an_array(void *a, int el, unsigned int len)
+{
+	char *p = a;
+	unsigned int i = 0;
+
+	while (i < len)
+	{
+		*p = el;
+		p++;
+		i++;
+	}
+	return (a);
+}
 
 /**
- * find_command - finds command to execute in path routes.
+ * array_rev - reverse array
+ * @arr: the given array
+ * @len: the array length
  *
- * @command: first position of getline input.
- *
- * Return: string of folder for command to be executed.
- **/
-char *find_command(char *command)
+ * Return: void
+ */
+void array_rev(char *arr, int len)
 {
-	DIR *folder;
-	struct dirent *entry;
-	char *cmd, comp, **str  = malloc(sizeof(char) * 1024);
-	char **split = malloc(sizeof(char) * 1024);
 	int i;
+	char tmp;
 
-	while (*environ != NULL)
+	for (i = 0; i < (len / 2); i++)
 	{
-		if (!(_strcmpdir(*environ, "PATH")))
-		{
-			*str = *environ;
-			for (i = 0; i < 9; i++, split++, str++)
-			{
-				*split = strtok(*str, ":='PATH'");
-				folder = opendir(*split);
-				if (folder == NULL)
-				{
-					perror("Unable to read directory");
-				}
-				while ((entry = readdir(folder)))
-				{
-					cmd = entry->d_name;
-					comp = _strcmpdir(cmd, command);
-					if (comp == 0)
-					{
-						return (*split);
-					}
-					if (cmd == NULL)
-					{
-						perror("Error");
-					}}}}
-		environ++;
+		tmp = arr[i];
+		arr[i] = arr[(len - 1) - i];
+		arr[(len - 1) - i] = tmp;
 	}
-	return ("Error: Not Found");
+}
+/**
+ * index_cmd - indexed command
+ * @data: a pointer to the data structure
+ *
+ * Return: void
+ */
+void index_cmd(sh_t *data)
+{
+	data->index += 1;
 }
